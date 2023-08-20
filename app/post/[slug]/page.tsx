cc4a872fa1698b43c2d654e75abf75dd14d444d3
ZIECOZENO4 <ziecozeno4@gmail.com>
2023-08-20 12:29:34 +0100

    res

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -4,24 +4,6 @@ import { Post } from '@/app/lib/interface';
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
-// async function getData() {
-//   const query = `*[_type == 'post]{
-//     _id,
-//     slug{
-//       current
-//     }
-//   }`;
-//   const data = await client.fetch(query)
-//   const paths = data.map((post:Post)=>({
-//     params:{
-//       slug: post.slug.current
-//     }
-//   }));
-//   return {
-//     paths,
-//     fallback: 'blocking',
-//   }
-// };
 const Post =  () =>{
   return (
     <div>
